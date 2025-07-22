376a55771bb5370694fa3d91bb7afd9ac7e1d0d0
Eitan Myron <eitanmyron@outlook.com>
2025-07-22 10:02:39 -0400

    Appointment models

--- appointments/models.py
+++ appointments/models.py
@@ -9,6 +9,7 @@ class Client(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
+    clinician = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clients', null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
